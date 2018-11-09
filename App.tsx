8af7a97a0f4e6aade5474bdd2def032fd9ed6197
Malene Langgaard <malenelanggaard@gmail.com>
2018-11-09 10:04:40 +0100

    user interface

--- App.tsx
+++ App.tsx
@@ -1,69 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, Image } from 'react-native';
-import { UserInfo } from './components/UserInfo';
+import React from "react";
+import { StyleSheet, Text, TextInput, Button, View, Image } from "react-native";
+import { UserInfo, User } from "./components/UserInfo";
 
-const apiUrl = "https://api.github.com"
+const apiUrl = "https://api.github.com";
 
 interface State {
-  token: string,
-  userName: string,
-  userInfo: {
-    login: string,
-    name: string,
-    avatar: string
-  }
-  noUser: boolean
+  token: string;
+  userName: string;
+  userInfo: User;
+  noUser: boolean;
 }
 
 export default class App extends React.Component<null, State> {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      token: '',
-      userName: '',
+      token: "",
+      userName: "",
       userInfo: null,
       noUser: false
-    }
+    };
   }
 
   getUserInfo = () => {
-      let user = this.state.userName
-        fetch( `${apiUrl}/users/${user}`, {
-         method: 'GET'
-      })
-      .then((response) => response.json())
+    let user = this.state.userName;
+    fetch(`${apiUrl}/users/${user}`, {
+      method: "GET"
+    })
+      .then(response => response.json())
       .then((data: any) => {
-        if(data.message) {
-          this.setState({userInfo: null, noUser: true})
+        if (data.message) {
+          this.setState({ userInfo: null, noUser: true });
         } else {
           this.setState({
-           userInfo: {
-            login: data.login,
-            name: data.name,
-            avatar: data.avatar_url
-           },
-           noUser: false
-         })
-        } 
+            userInfo: {
+              login: data.login,
+              name: data.name,
+              avatar: data.avatar_url
+            },
+            noUser: false
+          });
+        }
       })
-      .catch((error) => {
-         console.error(error);
+      .catch(error => {
+        console.error(error);
       });
-  }
+  };
 
   render() {
-    const {token, userInfo, userName, noUser} = this.state;
+    const { token, userInfo, userName, noUser } = this.state;
     return (
       <View style={styles.container}>
-        <Image style={styles.image} source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/768px-Ei-sc-github.svg.png"}}>
-          </Image>
-        <TextInput style={styles.input} placeholder="Type in username" value={userName} onChangeText={(value) => this.setState({userName: value})}/>
-        <Button onPress={this.getUserInfo} title="Get user"/>
-        {noUser && <Text style={styles.nouser}>No user found!</Text>}
-        {userInfo &&
-          <UserInfo userInfo={userInfo} style={styles.image}/>    
-        }
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/768px-Ei-sc-github.svg.png"
+          }}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Type in username"
+          value={userName}
+          onChangeText={value => this.setState({ userName: value })}
+        />
+        <Button
+          style={styles.getUser}
+          onPress={this.getUserInfo}
+          title="Get user"
+        />
+        {noUser && <Text style={styles.noUser}>No user found!</Text>}
+        {userInfo && <UserInfo user={userInfo} />}
       </View>
     );
   }
@@ -72,22 +79,23 @@ export default class App extends React.Component<null, State> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginTop: 50,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 50
   },
   image: {
-    width: 100, 
-    height: 100,
+    width: 100,
+    height: 100
   },
   input: {
-    height: 40,
-    fontSize: 40,
+    height: 30,
+    fontSize: 25
   },
-  nouser: {
+  getUser: {
+    marginTop: 10
+  },
+  noUser: {
     marginTop: 50,
     fontSize: 20
   }
-
-
 });
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,44 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export interface Props {
-    userInfo: {
-    login: string,
-    name: string,
-    avatar: string
-  }
-  style: {}
+export interface User {
+  login: string;
+  name: string;
+  avatar: string;
+}
+
+interface Props {
+  user: User;
 }
 export class UserInfo extends React.Component<Props, null> {
-
   constructor(props) {
-    super(props)
+    super(props);
   }
 
-render() {
-    const {userInfo, style} = this.props;
+  render() {
+    const { user } = this.props;
     return (
       <View style={styles.container}>
-          <Image
-          style={styles.image}
-          source={{uri: userInfo.avatar}}
-          />
-          <Text>{userInfo.login}</Text>
-           <Text>{userInfo.name}</Text>
+        <Image style={styles.image} source={{ uri: user.avatar }} />
+        <Text style={styles.userlogin}>{user.login}</Text>
+        <Text>{user.name}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({  
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginTop: 50,
-  },     
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 50
+  },
   image: {
-    width: 50, 
-    height: 50,
-    }
-  })
+    width: 80,
+    height: 80
+  },
+  userlogin: {
+    marginTop: 40,
+    fontSize: 20
+  }
+});
